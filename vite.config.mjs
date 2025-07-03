49687dd0fd460493a3b824c6222f07072bd5552c
Renato Quiroz <renatoquiroz5500@gmail.com>
2025-07-03 11:28:30 -0400

    Fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 export default defineConfig(() => {
   return {
-    base: process.env.VITE_BASE_URL || '/application-frontend',
+    base: '/',
     build: {
       outDir: 'build',
     },
